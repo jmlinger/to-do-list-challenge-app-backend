e8e6e11db72b1b52de9eb85f0f0b0b7f2f197077
jmlinger <jmlinger10@gmail.com>
2022-02-20 22:50:08 -0300

    build: estrutura de model e users.

---
+++ src/models/connection.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const DB_NAME = 'To_Do_List';
+const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/${DB_NAME}`;
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let connection = null;
+
+module.exports = () => {
+  try {
+    if (connection) return connection;
+    connection = mongoose.connect(MONGO_DB_URL, OPTIONS);
+    return connection;
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
